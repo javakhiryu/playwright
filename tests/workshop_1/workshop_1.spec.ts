3f631bb31d5b75690dbd4baeeea1c9a7fac7cbda
javakhiryu <136607495+javakhiryu@users.noreply.github.com>
2024-10-13 22:53:18 +0500

    playwright on TS project

---
+++ tests/workshop_1/workshop_1.spec.ts
@@ -0,0 +1,24 @@
+import {test} from 'playwright/test';
+
+test.skip('Basic Navigation', async ({page}) => {
+    await page.goto('https://gitlab.com/');
+    await page.waitForTimeout(3000);
+    await page.reload();
+})
+
+test.skip( 'Interacting with Web Element on Gitlab', async ({page}) => {
+    await page.goto('https://gitlab.com/');
+    await page.click(`//*[local-name()='svg'][@aria-labelledby="tanukiHomeDesktop"]`);
+    await page.locator('//a[@name="Get free trial"]').click();
+    await page.getByTestId('new-user-first-name-field').fill('John');
+    await page.getByTestId('new-user-last-name-field').fill('Doe');
+})
+
+test("Using Various Locator Methods", async ({page}) => {
+    await page.goto('https://gitlab.com/');
+    //await page.click(`//*[local-name()='svg'][@aria-labelledby="tanukiHomeDesktop"]`); //Gitlab logo button
+    //await page.locator(`(//*[name()='svg'])[33]`).click();
+    await page.click(':has-text("Sign in")')
+    await page.waitForTimeout(3000);
+})
+
